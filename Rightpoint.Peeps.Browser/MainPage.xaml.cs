a5309ee74772ce63b33906931a597c9165792150
Joe Meyer <JMeyer@rightpoint.com>
2017-05-05 12:05:30 -0500

    Init

--- Rightpoint.Peeps.Browser/MainPage.xaml.cs
+++ Rightpoint.Peeps.Browser/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +11,25 @@ namespace Rightpoint.Peeps.Browser
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        WebView content = new WebView();
+        Uri site = new Uri("http://rightpoint.com/tenyears");
         public MainPage()
         {
             this.InitializeComponent();
+
+            var refreshTimer = new DispatcherTimer();
+            refreshTimer.Tick += RefreshTimerOnTick;
+            refreshTimer.Interval = new TimeSpan(0, 0, 30, 0);
+            refreshTimer.Start();
+
+            content.Source = site;
+
+            this.Content = content;
+        }
+
+        private void RefreshTimerOnTick(object sender, object o)
+        {
+            content.Navigate(site);
         }
     }
 }
